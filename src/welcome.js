a99098ce0ce839df27043c30cf756f0304cc9aa7
Ashley Grant <ashley@ashleygrant.com>
2015-04-16 16:39:14 -0400

    Add example for SO Question

--- src/welcome.js
+++ src/welcome.js
@@ -1,25 +1,22 @@
-import {computedFrom} from 'aurelia-framework';
 
 export class Welcome{
-  heading = 'Welcome to the Aurelia Navigation App!';
-  firstName = 'John';
-  lastName = 'Doe';
+  constructor() {
+    let items = [];
 
-  //Getters can't be observed with Object.observe, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
-  //@computedFrom('firstName', 'lastName')
-  get fullName(){
-    return `${this.firstName} ${this.lastName}`;
-  }
+    for(let i = 0; i < 5; i++) {
+      items.push(new Item());
+    }
 
-  welcome(){
-    alert(`Welcome, ${this.fullName}!`);
+    this.items = items;
   }
 }
 
-export class UpperValueConverter {
-  toView(value){
-    return value && value.toUpperCase();
+class Item {
+  constructor(enabled) {
+    this.enabled = false;
+  }
+
+  toggleEnabled() {
+    this.enabled = !this.enabled;
   }
 }
